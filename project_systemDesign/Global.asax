3288d00ff5b40b2af80682820ac86f36e5ab7104
unknown <haneen2005.moh@gmail.com>
2026-04-26 00:30:26 +0300

    Project first commit - done: Log in / sign up

---
+++ project_systemDesign/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="project_systemDesign.MvcApplication" Language="C#" %>
---
+++ project_systemDesign/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(project_systemDesign.Startup))]
+namespace project_systemDesign
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
